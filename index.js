8289e84ef2f1ea9aa0e2cb8d1f6db614f6550fa4
000ashishYadav <000ashishyadav2003@gmail.com>
2025-05-29 23:09:16 +0530

    experience hello world example thriugh making our server

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("hello backend !");
+const express = require("express");
+const app = express();
+const port = 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/personal_info", (req, res) => {
+  res.send({
+    name: "Ashish Yadav",
+    age: 21,
+    contact_no: 8707456060,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
